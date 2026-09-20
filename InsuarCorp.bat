0f0616be7e9451dc48fa7f5d1055692b5efe3900
InsuarCorp <cl.insuar@gmail.com>
2026-09-20 16:59:05 -0300

    Add files via upload

---
+++ InsuarCorp.bat
@@ -0,0 +1,2 @@
+﻿@echo off
+powershell -Command "Start-Process powershell -ArgumentList '-NoExit', '-Command', 'irm https://raw.githubusercontent.com/InsuarCorp/W11/main/info.ps1 | iex' -Verb RunAs"
